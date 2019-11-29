6cfd689aa43cc3b24c0bbf27bbf059cbb5932885
Schulberg <jschulberg@deloitte.com>
2019-11-29 14:34:47 -0500

    Tried a random forest. Didn't work so well. Accuracy was generally around 80%. Converted everything to a factor.

--- Titanic Code.R
+++ Titanic Code.R
@@ -1,30 +1,12 @@
 # install.packages("tidyverse")
+# install.packages("randomForest")
+library(randomForest)
 library(tidyverse)
 library(ggplot2)
 library(stats)
 
 import <- read.csv("train.csv")
 test <- read.csv("test.csv")
-
-# sample line of code to see hsitory
-x <- 1
-
-# set the seed
-set.seed(123)
-
-# split our import data into training and validation data
-train_id <- sample(seq_len(nrow(import)), floor(nrow(import)*.8), replace = F)
-train <- import[import$PassengerId %in% train_id,]
-
-# validation data should have all the passenger IDs not in train data
-val <- import[!(import$PassengerId %in% train$PassengerId),]
-
-# ensure that we pulled enough data
-if (nrow(val) + nrow(train) == nrow(import) ) {
-  "Looks good"
-} else {
-  "Hold up wait a minute"
-}
 
 
 
@@ -33,31 +15,34 @@ if (nrow(val) + nrow(train) == nrow(import) ) {
 ###### Data Wrangling ######
 ############################
 # convert Sex to categorical numbers
-train$ind_sex[train$Sex == 'female'] <- 0
-train$ind_sex[train$Sex == 'male'] <- 1
+import$ind_sex[import$Sex == 'female'] <- 0
+import$ind_sex[import$Sex == 'male'] <- 1
+import$ind_sex <- as.factor(import$ind_sex)
 
 # take the ceiling of all the ages so we get rid of the half ages
-train$Age <- ceiling(train$Age)
+import$Age <- ceiling(import$Age)
 
 # convert age to a standardized value
-mean_age <- mean(train$Age, na.rm = T)
-sd_age <- sd(train$Age, na.rm = T)
-train$ind_age <- (train$Age - mean_age)/sd_age
+mean_age <- mean(import$Age, na.rm = T)
+sd_age <- sd(import$Age, na.rm = T)
+import$ind_age <- (import$Age - mean_age)/sd_age
 
 # convert Pclass to a factor
-train$Pclass <- as.factor(train$Pclass)
+import$Pclass <- as.factor(import$Pclass)
 
 # for cabin we only care about the floor they were on, which is the first letter
-train$ind_cabin <- substr(train$Cabin, 1, 1)
-train$ind_cabin[train$ind_cabin == ""] <- "Unknown"
-plot(prop.table(table(train$ind_cabin)))
+import$ind_cabin <- substr(import$Cabin, 1, 1)
+import$ind_cabin[import$ind_cabin == ""] <- "Unknown"
+plot(prop.table(table(import$ind_cabin)))
 
-train$ind_cabin <- as.factor(train$ind_cabin)
+import$ind_cabin <- as.factor(import$ind_cabin)
 
 # create family size column
-train <- train %>%
+import <- import %>%
   mutate(family_size = 1 + SibSp + Parch)
+import$family_size <- as.factor(import$family_size)
 
+import$Survived <- as.factor(import$Survived)
 
 
 
@@ -65,15 +50,13 @@ train <- train %>%
 #######     EDA     ########
 ############################
 
-str(train)
-
-train$Survived <- as.factor(train$Survived)
+str(import)
 
 # let's create data frames out of training data for those who survived and those who died
-survived <- train %>% 
+survived <- import %>% 
   filter(Survived == 1)
 
-dead <- train %>%
+dead <- import %>%
   filter(Survived == 0)
 
 # Sex is a pretty good indicator, it appears
@@ -106,7 +89,7 @@ legend(1.75, 15, c("Survived", "Dead"),
 
 # SibSp is not a good indicator by itself because 93% of people 
 # generally have less than 2 siblings
-prop.table(table(train$SibSp)) # 93% of all passengers have less than 2 siblings
+prop.table(table(import$SibSp)) # 93% of all passengers have less than 2 siblings
 
 table(survived$SibSp)
 prop.table(table(survived$SibSp)) # 85% of survivors have less than 2 siblings
@@ -119,7 +102,7 @@ hist(dead$SibSp)
 
 # Number of parents / children aboard the Titanic is not a good indicator by 
 # itself because 90% of people generally have less than 2 parents/children aboard
-prop.table(table(train$Parch)) # 90% of all passengers have less than 2 siblings
+prop.table(table(import$Parch)) # 90% of all passengers have less than 2 siblings
 
 table(survived$Parch)
 prop.table(table(survived$Parch)) # 87% of survivors have less than 2 siblings
@@ -141,8 +124,8 @@ survived %>%
 
 # Class of passenger aboard the Titanic is not a good indicator by 
 # itself because 90% of people generally have less than 2 parents/children aboard
-prop.table(table(train$Pclass)) # 55% are class 3 (lowest class)
-hist(train$Pclass)
+prop.table(table(import$Pclass)) # 55% are class 3 (lowest class)
+hist(import$Pclass)
 
 table(survived$Pclass)
 prop.table(table(survived$Pclass)) # only 37% of survivors are class 3
@@ -154,16 +137,16 @@ hist(dead$Pclass)
 
 # Family size aboard the Titanic is not a good indicator by 
 # itself because 90% of people generally have less than 2 parents/children aboard
-prop.table(table(train$family_size)) # 55% are class 3 (lowest class)
-hist(train$family_size)
+prop.table(table(import$family_size)) # 55% are class 3 (lowest class)
+hist(import$family_size)
 
-table(survived$family_size)
+table(survived$family_size) # the largest family size for survivors is 7 (4 people)
 prop.table(table(survived$family_size)) # 76% of survivors have family size <3
-hist(survived$family_size) # the largest family size
+hist(survived$family_size) 
 
-table(dead$family_size)
+table(dead$family_size) # the largest family size for dead is 11 (8 people have greater than 8)
 prop.table(table(dead$family_size)) # 82% of dead have family size <3
-hist(dead$family_size)
+hist(dead$family_size) 
 
 
 
@@ -171,8 +154,57 @@ hist(dead$family_size)
 ######     Model     #######
 ############################
 
+# set the seed
+set.seed(123)
+
+# split our import data into training (80%) and validation (20%) data
+train_id <- sample(seq_len(nrow(import)), floor(nrow(import)*.8), replace = F)
+train <- import[import$PassengerId %in% train_id,]
+
+# validation data should have all the passenger IDs not in train data
+val <- import[!(import$PassengerId %in% train$PassengerId),]
+
+# only bring in columns that we can model on
+str(train)
+train <- train %>%
+  select(Survived, Pclass, ind_sex, ind_cabin, family_size)
+
+
+# ensure that we pulled enough data
+if (nrow(val) + nrow(train) == nrow(import) ) {
+  "Looks good"
+} else {
+  "Hold up wait a minute"
+}
+
+
 fit_1 <- lm(train$Survived ~ train$Age)
 summary(fit_1) # gives a high p value of .1841 -- probably not a good variable to use
 
 fit_2 <- lm(train$Survived ~ train$Fare)
 summary(fit_2) # gives a low p-value (8.32e-13) but the r^2 value is veryyy low
+
+# Let's try out our first randomForest. This gives an OOB estimate of error rate 19.5%.
+# The class errors seem to be pretty large as well. I'm not sure this worked as well as
+# I would have liked.
+(model1 <- randomForest(Survived ~ ., data = train, importance = TRUE, proximity = TRUE))
+
+# Let's try out our second randomForest where we have tried 4 variables at each split.
+# This gives an OOB estimate of error rate 22%. The class errors seem to be even
+# larger than the previous model. I'm not sure this worked as well as I would have liked.
+(model2 <- randomForest(Survived ~ ., data = train, importance = TRUE, mtry = 4))
+
+
+# let's check how this worked on our train set
+# Predicting on train set
+pred_train <- predict(model1, train, type = "class")
+# Checking classification accuracy
+table(pred_train, train$Survived)  
+
+# Predicting on validation set
+pred_val <- predict(model1, val, type = "class")
+# Checking classification accuracy
+table(pred_val, val$Survived)  
+
+# find the overall accuracy by using the mean
+mean(pred_val == val$Survived) # accuracy comes out to 79.9%
